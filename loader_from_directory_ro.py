28d922e222d3f77c97b9c1598e275d1761c3a077
Stroescu Marius <stroescump@gmail.com>
2020-04-11 22:59:37 +0300

    Managed to plot the confusion matrix for a certain training, still need to go through Validation and Testing

--- loader_from_directory_ro.py
+++ loader_from_directory_ro.py
@@ -7,6 +7,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 from PIL import Image
+from sklearn.metrics import confusion_matrix
+from final_model.resources import plot_cmatrix
 
 torch.cuda.init()
 # print(torch.cuda.is_available())
@@ -46,6 +48,7 @@ for simbol in simboluri:
 for simbol in simboluri:
     matrice_encoding[simbol][poz] = 1
     poz += 1
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 data = []
 
@@ -62,11 +65,10 @@ for tgt in os.listdir("dataset"):
             image = cv2.resize(image, (28, 28))
             # Normalizare 0-1
             image = image.astype(np.float32) / 255.0
-            image = torch.from_numpy(image).to(
-                torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+            image = torch.from_numpy(image).cuda()
             picture.append(image)
             # Convertim echivalentul cuvantului in binar ca tensor si apoi il appenduim la picture
-            curr_target = torch.Tensor([curr_target])
+            curr_target = torch.Tensor([curr_target]).cuda()
             picture.append(curr_target)
             # Astfel fiecare pozitie din vectorul data contine 2 atribute: imaginea ca tensor si numele acesteia tensor
             data.append(picture)
@@ -85,7 +87,7 @@ for caracter in caractere:
 def num_chars(dataset, index2char):
     chars = {}
     for _, label in dataset:
-        char = index2char[int(torch.argmax(label))]
+        char = index2char[int(torch.argmax(label).to(device))]
         # update
         if char in chars:
             chars[char] += 1
@@ -107,22 +109,25 @@ test_dataset = data[22000:24400]
 validation_dataset = data[24400:]
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size_train, shuffle=True)
+# train_loader1 are parametrul Shuffle setat pe False pentru a ajuta la alcatuirea unei matrici de confuzie
+train_loader1 = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size_train, shuffle=False)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size_test, shuffle=False)
 validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=batch_size_validation,
                                                 shuffle=True)
 
-# Verificam integritatea datasetului dpdv al nr.de caractere
-test_chars = num_chars(test_dataset, index2char)
 
-num = 0
-summ = 0
-for caracter in caractere:
-    if caracter in test_chars:
-        summ+=test_chars[caracter]
-        num += 1
-    else:
-        break
-print(num)
+# Verificam integritatea datasetului dpdv al nr.de caractere
+# test_chars = num_chars(test_dataset, index2char)
+#
+# num = 0
+# summ = 0
+# for caracter in caractere:
+#     if caracter in test_chars:
+#         summ += test_chars[caracter]
+#         num += 1
+#     else:
+#         break
+# print(num)
 
 
 class CNN(nn.Module):
@@ -131,7 +136,7 @@ class CNN(nn.Module):
         self.block1 = nn.Sequential(
             # 3x28x28
             nn.Conv2d(in_channels=3,
-                      out_channels=16,
+                      out_channels=20,
                       kernel_size=5,
                       stride=1,
                       padding=2),
@@ -142,8 +147,8 @@ class CNN(nn.Module):
         )
         # 16x14x14
         self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels=16,
-                      out_channels=32,
+            nn.Conv2d(in_channels=20,
+                      out_channels=40,
                       kernel_size=5,
                       stride=1,
                       padding=2),
@@ -154,7 +159,7 @@ class CNN(nn.Module):
         )
         # linearly 
         self.block3 = nn.Sequential(
-            nn.Linear(32 * 7 * 7, 100),
+            nn.Linear(40 * 7 * 7, 100),
             nn.LeakyReLU(),
             nn.Linear(100, 37)
         )
@@ -164,26 +169,83 @@ class CNN(nn.Module):
         out = self.block1(x)
         out = self.block2(out)
         # Transformam tensorul de grad superior intr-unul de grad 1, liniar
-        out = out.view(-1, 32 * 7 * 7)
+        out = out.view(-1, 40 * 7 * 7)
         out = self.block3(out)
         return out
 
 
 # CNN Model utilizand placa grafica - RTX2070
-cuda_ = "cuda:0"
-device = torch.device(cuda_ if torch.cuda.is_available() else "cpu")
 model = CNN()
 model.to(device)
+
+
+@torch.no_grad()
+def get_all_preds(modelCNN, loader):
+    all_preds = torch.tensor([]).cuda()
+    # auxLabelsTensor = torch.tensor([[[]]]).cuda()
+    for batch in loader:
+        images, auxLabelsTensor = batch
+        images = images.permute(0, 3, 1, 2)
+        # loadLabelsInThisVariable = torch.cat((loadLabelsInThisVariable, auxLabelsTensor))
+        preds = modelCNN(images)
+        all_preds = torch.cat(
+            (all_preds, preds),
+            dim=0
+        ).cuda()
+    return all_preds
+
+
+def getAllLabelsAsTensor(inputDataTrainingTensor):
+    labelsTensor = torch.FloatTensor().cuda()
+    for batch in inputDataTrainingTensor:
+        _, labels1 = batch
+        labels1 = labels1.view(-1, 37).type(torch.FloatTensor).cuda()
+        labelsTensor = torch.cat((labelsTensor, labels1), dim=0).cuda()
+    return labelsTensor
+
+
+# TODO : Seems to be working but I still think there s something off with this func.
+def get_correct_predictions(trained_output, inputLabelTensor):
+    correct1 = torch.FloatTensor().cuda()
+    trained_output = trained_output.argmax(dim=1)
+    correct1 = trained_output.eq(labelTensor).sum()
+    return correct1
+
+
+def create_and_display_confusion_matrix():
+    labelTensor = getAllLabelsAsTensor(train_loader1).to(device)
+    train_predictions = get_all_preds(model, train_loader1).to(device)
+
+    labelTensor = torch.argmax(labelTensor, dim=1).to(device)
+    stackedTensor = torch.stack((labelTensor, train_predictions.argmax(dim=1)), dim=1)
+    conf_matrix = torch.zeros(37, 37, dtype=torch.int32)
+    # print(conf_matrix)
+
+    for pair in stackedTensor:
+        true_label, predicted_label = pair.tolist()
+        conf_matrix[true_label, predicted_label] = conf_matrix[true_label, predicted_label] + 1
+    print(conf_matrix)
+
+    namesForCMatrix = (
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w',
+        'x', 'y', 'z', "space", "number", "period", "comma", "colon", "apostrophe", "hyphen", "semicolon", "question",
+        "exclamation", "capitalize")
+    plt.figure(figsize=(20, 20))
+    plot_cmatrix.plot_confusion_matrix(conf_matrix, namesForCMatrix)
+
+
+# torch.sum(torch.eq(inputLabelTensor, trained_output))
 
 # Pre-verificare a modelului
 print(model)
 print("# parameter: ", sum([param.nelement() for param in model.parameters()]))
 
 # Rata de invatare
-learning_rate = 1e-4
+learning_rate = 0.001
 
 # Using a variable to store the cross entropy method
-criterion = nn.CrossEntropyLoss().to(device)
+criterion = nn.CrossEntropyLoss().cuda()
 
 # Using a variable to store the optimizer
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -195,7 +257,7 @@ train_losses = []
 validation_losses = []
 
 # for loop that iterates over all the epochs
-num_epochs = 200
+num_epochs = 5
 for epoch in range(num_epochs):
 
     # variables to store/keep track of the loss and number of iterations
@@ -204,37 +266,24 @@ for epoch in range(num_epochs):
 
     # train the model
     model.train()
-
-    # Iterate over train_loader
-    for i, (images, labels) in enumerate(train_loader):
-        # need to permute so that the images are of size 3x28x28 
-        # essential to be able to feed images into the model
+    for batch in train_loader:
+        images, labels = batch
+        # Permutam tensorul pentru a corespunde cu inputul modelului [batch_size,channel,width,height]
         images = images.permute(0, 3, 1, 2)
-
-        # Zero the gradient buffer
-        # resets the gradient after each epoch so that the gradients don't add up
-        optimizer.zero_grad()
-
-        # Forward, get output
         outputs = model(images)
-
-        # convert the labels from one hot encoding vectors into integer values
-        cuda_ = "cuda:0"
-        device = torch.device(cuda_ if torch.cuda.is_available() else "cpu")
+        # Modelam tensorul ce contine labels, facem squeeze pentru a putea avea dimensiunea asteptata de LossFunc.
         labels = labels.view(-1, 37)
-        y_true = torch.argmax(labels, 1).to(device)
-
-        # calculate training loss
-        loss = criterion(outputs, y_true).to(device)
-
+        labelsConverted = torch.argmax(labels, dim=1).cuda()
+        # Calculam functia de cost
+        loss = criterion(outputs, labelsConverted).cuda()
         # Backward (computes all the gradients)
+        optimizer.zero_grad()
         loss.backward()
-
         # Optimize
-        # loops through all parameters and updates weights by using the gradients 
-        # takes steps backwards to optimize (to reach the minimum weight)
+        # Loops through all parameters and updates weights by using the gradients
+        # Takes steps backwards to optimize (to reach the minimum weight)
         optimizer.step()
-        # update the training loss and number of iterations
+        # Update the training loss and number of iterations
         train_loss += loss.data
         num_iter_train += 1
 
@@ -243,71 +292,75 @@ for epoch in range(num_epochs):
     # append training loss over all the epochs
     train_losses.append(train_loss / num_iter_train)
 
-    # evaluate the model
-    model.eval()
+# Uncomment the following line to plot the confusion matrix corresponding to a training session
+create_and_display_confusion_matrix()
 
-    # variables to store/keep track of the loss and number of iterations
-    validation_loss = 0
-    num_iter_validation = 0
+# TODO: Do not delete these lines, they account for the evaluation part of the model
 
-    # Iterate over validation_loader
-    for i, (images, labels) in enumerate(validation_loader):
-        # need to permute so that the images are of size 3x28x28 
-        # essential to be able to feed images into the model
-        images = images.permute(0, 3, 1, 2)
+# evaluate the model
+# model.eval()
 
-        # Forward, get output
-        outputs = model(images)
-
-        # convert the labels from one hot encoding vectors to integer values
-        labels = labels.view(-1, 37)
-        y_true = torch.argmax(labels, 1).to(device)
-
-        # calculate the validation loss
-        loss = criterion(outputs, y_true).to(device)
-
-        # update the training loss and number of iterations
-        validation_loss += loss.data
-        num_iter_validation += 1
-
-    print('Validation Loss: {:.4f}'.format(validation_loss / num_iter_validation))
-    # append all validation_losses over all the epochs
-    validation_losses.append(validation_loss / num_iter_validation)
-
-    num_iter_test = 0
-    correct = 0
-
-    # Iterate over test_loader
-    for images, labels in test_loader:
-        # need to permute so that the images are of size 3x28x28 
-        # essential to be able to feed images into the model
-        images = images.permute(0, 3, 1, 2)
-
-        # Forward
-        outputs = model(images)
-
-        # convert the labels from one hot encoding vectors into integer values 
-        labels = labels.view(-1, 37)
-        y_true = torch.argmax(labels, 1).to(device)
-
-        # find the index of the prediction
-        y_pred = torch.argmax(outputs, 1).type('torch.FloatTensor').to(device)
-
-        # convert to FloatTensor
-        y_true = y_true.type('torch.FloatTensor').to(device)
-
-        # find the mean difference of the comparisons
-        correct += torch.sum(torch.eq(y_true, y_pred).type('torch.FloatTensor')).to(device)
-    x = (correct / len(test_dataset) * 100)
-
-    nume = str('{:.4f}'.format(x)) + "model_1_final_highAccuracy.pth"
-
-    if x > 99.89:
-        torch.save(model.state_dict(), nume)
-        print('Saved!')
-
-    print('Accuracy on the test set: {:.4f}%'.format(correct / len(test_dataset) * 100))
-    print()
+# # variables to store/keep track of the loss and number of iterations
+# validation_loss = 0
+# num_iter_validation = 0
+#
+# # Iterate over validation_loader
+# for i, (images, labels) in enumerate(validation_loader):
+#     # need to permute so that the images are of size 3x28x28
+#     # essential to be able to feed images into the model
+#     images = images.permute(0, 3, 1, 2)
+#
+#     # Forward, get output
+#     outputs = model(images)
+#
+#     # convert the labels from one hot encoding vectors to integer values
+#     labels = labels.view(-1, 37)
+#     y_true = torch.argmax(labels, 1).to(device)
+#
+#     # calculate the validation loss
+#     loss = criterion(outputs, y_true).to(device)
+#
+#     # update the training loss and number of iterations
+#     validation_loss += loss.data
+#     num_iter_validation += 1
+#
+# print('Validation Loss: {:.4f}'.format(validation_loss / num_iter_validation))
+# # append all validation_losses over all the epochs
+# validation_losses.append(validation_loss / num_iter_validation)
+#
+# num_iter_test = 0
+# correct = 0
+#
+# # Iterate over test_loader
+# for images, labels in test_loader:
+#     images = images.permute(0, 3, 1, 2)
+#
+#     # Forward
+#     outputs = model(images)
+#
+#     # convert the labels from one hot encoding vectors into integer values
+#     labels = labels.view(-1, 37)
+#     y_true = torch.argmax(labels, 1).to(device)
+#
+#     # find the index of the prediction
+#     y_pred = torch.argmax(outputs, 1).type('torch.FloatTensor').to(device)
+#
+#     # convert to FloatTensor
+#     y_true = y_true.type('torch.FloatTensor').to(device)
+#
+#     # find the mean difference of the comparisons
+#     correct += torch.sum(torch.eq(y_true, y_pred).type('torch.FloatTensor')).to(device)
+#
+# x = (correct / len(test_dataset) * 100)
+# print(correct)
+# nume = str('{:.4f}'.format(x)) + "model_1_final_highAccuracy.pth"
+#
+# if x > 99.89:
+#     torch.save(model.state_dict(), nume)
+#     print('Saved!')
+#
+# print('Accuracy on the test set: {:.4f}%'.format(correct / len(test_dataset) * 100))
+# print()
 
 # learning curve function
 # def plot_learning_curve(train_losses, validation_losses):
@@ -319,12 +372,10 @@ for epoch in range(num_epochs):
 #     plt.legend(loc=1)
 #     plt.show()
 
-
 # plot the learning curve
 # plt.title("Learning Curve (Loss vs Number of Epochs)")
 # plot_learning_curve(train_losses, validation_losses)
 # input_names = [ "actual_input_1" ] + [ "learned_%d" % i for i in range(16) ]
 # output_names = [ "output1" ]
-
 
 # torch.save(model.state_dict(), "model_1_final_1.pth")
